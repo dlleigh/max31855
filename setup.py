8f8877d5f74edd29a0c05fab4b1e4284ac7f731a
David Leigh <dleigh@us.ibm.com>
2025-03-02 15:57:48 -0500

    remove quotes around name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='max31855',
+    name=max31855,
     version='2.0',
     description='Python driver for MAX31855 Cold-Junction Compensated Thermocouple-to-Digital Converter',
     url='https://github.com/Tuckie/max31855',
